5c011d7e2041e6aff65fccd6a7076323c66d94f8
HengLuRepos <1505613731@qq.com>
2024-02-07 16:47:33 -0500

    update

--- plot_all.sh
+++ plot_all.sh
@@ -4,9 +4,11 @@ folders=("DDPG" "TD3" "SAC-cleanrl" "PPO-cleanrl" "TRPO")
 
 
 envs=("HalfCheetah-v4" "Ant-v4" "HumanoidStandup-v4" "Hopper-v4" "Humanoid-v4")
-mkdir figs/l1
-mkdir figs/l2
+
 for folder in "${folders[@]}"; do
+    rm "$folder/figs/*.png"
+    mkdir "$folder/figs/l1"
+    mkdir "$folder/figs/l2"
     for env in "${envs[@]}"; do
         python plot_prun.py --algs "$folder" --env-id "$env" --n 1
         python plot_prun.py --algs "$folder" --env-id "$env" --n 2

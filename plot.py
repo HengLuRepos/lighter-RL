33128a537d4cdaf3cf19d84710359ec45e025b01
HengLuRepos <1505613731@qq.com>
2024-04-25 10:32:34 -0400

    update

--- plot.py
+++ plot.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
+from matplotlib.ticker import FuncFormatter
+def format_y(value, tick_number):
+    return f'{value:.2f}'
+formatter = FuncFormatter(format_y)
 
 def parse_args():
     # fmt: off
@@ -15,15 +19,15 @@ def parse_args():
     return args
 args = parse_args()
 
-
+plt.rcParams['font.size'] = 16
 envs = ["HalfCheetah-v4", "HumanoidStandup-v4", "Ant-v4", "Hopper-v4", "Humanoid-v4"]
 methods = ("Baseline", "PTDQ", "PTSQ", "QAT")
 
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} l{args.n} avg return")
-fig.supxlabel("prune amount")
-fig.supylabel("scaled average return")
+#fig.suptitle(f"{args.algs} l{args.n} avg return")
+#fig.supxlabel("prune amount")
+#fig.supylabel("scaled average return")
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/{env}-l{args.n}.csv"
   data = pd.read_csv(path, header=None)
@@ -36,14 +40,14 @@ for idx, env in enumerate(envs):
   axs[idx].plot(x, avg)
   axs[idx].set_title(env)
   axs[idx].errorbar(x, avg, yerr=avg_std)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/l{args.n}/return/{args.algs}-l{args.n}-pruning-avg-return.png")
+  axs[idx].yaxis.set_major_formatter(formatter)
+fig.subplots_adjust(hspace=0.6,left=0.1,right=0.95)
+plt.savefig(f"figs/l{args.n}/return/{args.algs}-l{args.n}-pruning-avg-return.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} l{args.n} inference time")
-fig.supxlabel("prune amount")
-fig.supylabel("scaled inference time")
+#fig.suptitle(f"{args.algs} l{args.n} avg return")
+#fig.supxlabel("prune amount")
+#fig.supylabel("scaled average return")
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/{env}-l{args.n}.csv"
   data = pd.read_csv(path, header=None)
@@ -56,14 +60,14 @@ for idx, env in enumerate(envs):
   axs[idx].plot(x, avg)
   axs[idx].set_title(env)
   axs[idx].errorbar(x, avg, yerr=avg_std)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/l{args.n}/infer/{args.algs}-l{args.n}-pruning-infer-time.png")
+  axs[idx].yaxis.set_major_formatter(formatter)
+fig.subplots_adjust(hspace=0.6,left=0.1,right=0.95)
+plt.savefig(f"figs/l{args.n}/infer/{args.algs}-l{args.n}-pruning-infer-time.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} l{args.n} episodic length")
-fig.supxlabel("prune amount")
-fig.supylabel("scaled episodic length")
+#fig.suptitle(f"{args.algs} l{args.n} avg return")
+#fig.supxlabel("prune amount")
+#fig.supylabel("scaled average return")
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/{env}-l{args.n}.csv"
   data = pd.read_csv(path, header=None)
@@ -76,14 +80,14 @@ for idx, env in enumerate(envs):
   axs[idx].plot(x, avg)
   axs[idx].set_title(env)
   axs[idx].errorbar(x, avg, yerr=avg_std)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/l{args.n}/len/{args.algs}-l{args.n}-pruning-ep-len.png")
+  axs[idx].yaxis.set_major_formatter(formatter)
+fig.subplots_adjust(hspace=0.6,left=0.1,right=0.95)
+plt.savefig(f"figs/l{args.n}/len/{args.algs}-l{args.n}-pruning-ep-len.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} l{args.n} ram usage")
-fig.supxlabel("prune amount")
-fig.supylabel("scaled ram usage")
+#fig.suptitle(f"{args.algs} l{args.n} avg return")
+#fig.supxlabel("prune amount")
+#fig.supylabel("scaled average return")
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/{env}-l{args.n}.csv"
   data = pd.read_csv(path, header=None)
@@ -96,14 +100,14 @@ for idx, env in enumerate(envs):
   axs[idx].plot(x, avg)
   axs[idx].set_title(env)
   axs[idx].errorbar(x, avg, yerr=avg_std)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/l{args.n}/ram/{args.algs}-l{args.n}-pruning-ram.png")
+  axs[idx].yaxis.set_major_formatter(formatter)
+fig.subplots_adjust(hspace=0.6,left=0.1,right=0.95)
+plt.savefig(f"figs/l{args.n}/ram/{args.algs}-l{args.n}-pruning-ram.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} l{args.n} energy usage")
-fig.supxlabel("prune amount")
-fig.supylabel("scaled energy usage")
+#fig.suptitle(f"{args.algs} l{args.n} avg return")
+#fig.supxlabel("prune amount")
+#fig.supylabel("scaled average return")
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/{env}-l{args.n}.csv"
   data = pd.read_csv(path, header=None)
@@ -115,14 +119,14 @@ for idx, env in enumerate(envs):
   axs[idx].plot(x, avg)
   axs[idx].set_title(env)
   axs[idx].errorbar(x, avg, yerr=avg_std)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/l{args.n}/energy/{args.algs}-l{args.n}-pruning-energy.png")
+  axs[idx].yaxis.set_major_formatter(formatter)
+fig.subplots_adjust(hspace=0.6,left=0.1,right=0.95)
+plt.savefig(f"figs/l{args.n}/energy/{args.algs}-l{args.n}-pruning-energy.png",dpi=600)
 
 """
+
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} onnx inference time")
-fig.supylabel("scaled inference time")
+
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/onnxQuant/{env}.csv"
   data = pd.read_csv(path, header=None)
@@ -133,13 +137,12 @@ for idx, env in enumerate(envs):
   
   axs[idx].bar(methods, avg, yerr=avg_std)
   axs[idx].set_title(env)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/onnxQuant/infer/{args.algs}-infer.png")
+fig.subplots_adjust(hspace=0.6,left=0.05,right=0.95)
+#fig.legend()
+plt.savefig(f"figs/onnxQuant/infer/{args.algs}-infer.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} onnx average return")
-fig.supylabel("scaled average return")
+
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/onnxQuant/{env}.csv"
   data = pd.read_csv(path, header=None)
@@ -150,13 +153,12 @@ for idx, env in enumerate(envs):
   
   axs[idx].bar(methods, avg, yerr=avg_std)
   axs[idx].set_title(env)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/onnxQuant/return/{args.algs}-return.png")
+fig.subplots_adjust(hspace=0.6,left=0.05,right=0.95)
+#fig.legend()
+plt.savefig(f"figs/onnxQuant/return/{args.algs}-return.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} onnx episodic length")
-fig.supylabel("scaled episodic length")
+
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/onnxQuant/{env}.csv"
   data = pd.read_csv(path, header=None)
@@ -167,13 +169,12 @@ for idx, env in enumerate(envs):
   
   axs[idx].bar(methods, avg, yerr=avg_std)
   axs[idx].set_title(env)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/onnxQuant/len/{args.algs}-length.png")
+fig.subplots_adjust(hspace=0.6,left=0.05,right=0.95)
+#fig.legend()
+plt.savefig(f"figs/onnxQuant/len/{args.algs}-length.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} onnx ram usage")
-fig.supylabel("scaled ram usage")
+
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/onnxQuant/{env}.csv"
   data = pd.read_csv(path, header=None)
@@ -184,13 +185,12 @@ for idx, env in enumerate(envs):
   
   axs[idx].bar(methods, avg, yerr=avg_std)
   axs[idx].set_title(env)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/onnxQuant/ram/{args.algs}-ram.png")
+fig.subplots_adjust(hspace=0.6,left=0.05,right=0.95)
+#fig.legend()
+plt.savefig(f"figs/onnxQuant/ram/{args.algs}-ram.png",dpi=600)
 
 fig, axs = plt.subplots(5, sharex=True)
-fig.suptitle(f"{args.algs} onnx energy")
-fig.supylabel("scaled energy")
+
 for idx, env in enumerate(envs):
   path = f"{args.algs}/csv/onnxQuant/{env}.csv"
   data = pd.read_csv(path, header=None)
@@ -200,7 +200,8 @@ for idx, env in enumerate(envs):
   
   axs[idx].bar(methods, avg, yerr=avg_std)
   axs[idx].set_title(env)
-fig.subplots_adjust(hspace=0.6)
-fig.legend()
-plt.savefig(f"figs/onnxQuant/energy/{args.algs}-energy.png")
+fig.subplots_adjust(hspace=0.6,left=0.05,right=0.95)
+#fig.legend()
+plt.savefig(f"figs/onnxQuant/energy/{args.algs}-energy.png",dpi=600)
+
 """
